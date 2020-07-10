074dff84a5e57fb74cb2247fd52867060b483e86
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2020-07-10 18:13:33 +0200

    query text and image from wordpress for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,54 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+
+
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  {
+    allWordpressWpTeamHome{
+      edges{
+        node{title
+          featured_media{
+         localFile{
+          childImageSharp {
+            fluid(maxWidth: 300, maxHeight: 300) {
+              ...GatsbyImageSharpFluid
+              srcSet
+            }
+          }
+        }
+            source_url
+            
+          }
+          acf {
+            job_title
+            biography
+          }
+        }
+      }
+    }
+  }
+  `)
+  return (
+<div>
+  <h1>{data.allWordpressWpTeamHome.edges[0].node.title}</h1>
+  <div className ="imgContainer">
+  <Img
+                    alt=""
+                    fluid={data.allWordpressWpTeamHome.edges[0].node.featured_media.localFile.childImageSharp.fluid}
+                    
+                  />
+    
+    </div> 
+</div> 
+
+)}
 
 export default IndexPage
