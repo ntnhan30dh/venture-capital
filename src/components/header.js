82dc936ac4f6628088f259eb5526135bf3645241
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2020-07-16 15:24:45 +0200

    hide menu by default for mobile version

--- src/components/header.js
+++ src/components/header.js
@@ -6,22 +6,30 @@ import hamburger from "../images/hamburger.svg"
 import logo from "../images/logo.svg"
 
 class Header extends React.Component{
-	state = { showMenu: false }
+	state = { 
+		showMenu: false 
+	}
 	  toggleMenu = () => {
 		this.setState({
 		  showMenu: !this.state.showMenu
 		})
 		this.props.toggleNudgeHandle()
 	  }
+	  componentDidMount () {
+		if (window.matchMedia('(max-width: 767px)').matches) {
+		this.toggleMenu()
+		}
+	  }
 	render() {
-		const menuActive = this.state.showMenu ? 'is-inactive' : '';
+		let menuActive = this.state.showMenu ? 'is-inactive' : '';
+		
 		return (
   <nav id="navbar" className="nav" >
 		<div id="logo-cont" className="logo-cont cont">
 			<picture id="logo" className="logo">
 				<Link to="/"><img src={logo} height="45" alt="logo"/>
 				</Link></picture>
-			<div id="nav-menu" className={`nav-menu ${menuActive}`}>
+			<div id="nav-menu" className={`nav-menu navMobile ${menuActive}`}>
 				<Link to="/team"><div id="navteam" class="nav-element">TEAM</div></Link>
 				<Link to="/portfolio"><div id="navportfolio" class="nav-element">PORTFOLIO</div></Link>
 				<Link to="/strengths"><div id="navstrengths" class="nav-element">OUR STRENGTHS</div></Link>
