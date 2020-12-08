852f4e526b62143a5d0b7b2732dc738183bcfa4e
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2020-12-08 17:51:16 +0100

    add gtm with plugin and finalising newsroom

--- src/html.js
+++ src/html.js
@@ -4,14 +4,15 @@ import PropTypes from "prop-types"
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
-    <script dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      {/* <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PCWT8GP');</script>`,
-    }}
-/>
+})(window,document,'script','dataLayer','GTM-PCWT8GP');`,
+        }}
+      /> */}
 
       <head>
         <meta charSet="utf-8" />
@@ -22,6 +23,12 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
         {props.headComponents}
       </head>
+      {/* <noscript dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PCWT8GP" height="0" width="0"
+        style="display:none;visibility:hidden"></iframe>
+        `,
+    }}
+/> */}
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <div
--- src/pages/news.js
+++ src/pages/news.js
@@ -46,21 +46,30 @@ const NewsPage = () => {
       }
     }
   `)
-  const allPosts = data.allWordpressWpNews.edges
   const emptyQuery = ""
   const [state, setState] = useState({
     filteredData: [],
     query: emptyQuery,
+    numberOfPosts: 3
   })
+  const allPosts = data.allWordpressWpNews.edges.slice(0,state.numberOfPosts)
+
+  const increaseNumberOfPosts = ()=>{
+    const numberOfPosts = state.numberOfPosts+1
+    setState({
+      numberOfPosts
+    })
+  }
   const handleInputChange = event => {
-   // console.log(event.target.value)
+    // console.log(event.target.value)
+    const numberOfPosts = state.numberOfPosts
     const query = event.target.value
     const posts = data.allWordpressWpNews.edges || []
     const filteredData = posts.filter(post => {
-      const { title} = post.node
+      const { title } = post.node
       return (
         // description.toLowerCase().includes(query.toLowerCase()) ||
-        title.toLowerCase().includes(query.toLowerCase()) 
+        title.toLowerCase().includes(query.toLowerCase())
         // ||
         // (tags &&
         //   tags
@@ -72,6 +81,7 @@ const NewsPage = () => {
     setState({
       query,
       filteredData,
+      numberOfPosts
     })
   }
   const { filteredData, query } = state
@@ -81,47 +91,54 @@ const NewsPage = () => {
   return (
     <Layout>
       <div className="news">
-      <h1>Recent articles</h1>
-      <div className="searchBox">
-        <input
-          className="searchInput"
-          type="text"
-          aria-label="Search"
-          placeholder="Type to filter posts..."
-         onChange={handleInputChange}
-        />
-      </div>
-      <div class="team-cont">
-        {posts
-          .sort((a, b) =>
-            a.node.date < b.node.date
-              ? 1
-              : b.node.wordpress_id > a.node.wordpress_id
-              ? -1
-              : 0
-          ).slice(0,9)
-          .map(i => {
-            const date = new Date(i.node.date)
-            return (
-              <a class="news-card" href={`/post/${i.node.slug}`}>
-                <div class="news-picture">
-                  {/* <img
-                    src={
-                      i.node.featured_media.localFile.childImageSharp.fluid.src
-                    }
-                  /> */}
-                </div>
-                <div class="news-text">
-                  <p class="date">
-                    -{monthNames[date.getMonth()]} {date.getDate()},{" "}
-                    {date.getFullYear()}
-                  </p>
-                  <p class="excerpt">{i.node.acf.excerpt}</p>
-                </div>
-              </a>
+        <div className="top">
+          <h1>Recent articles</h1>
+          <h2>{state.numberOfPosts}</h2>
+          <div className="searchBox">
+            <input
+              className="searchInput"
+              type="text"
+              aria-label="Search"
+              onChange={handleInputChange}
+              value={state.query}
+            />
+          </div>
+        </div>
+        <div class="new-cards">
+          {posts
+            .sort((a, b) =>
+              a.node.date < b.node.date
+                ? 1
+                : b.node.wordpress_id > a.node.wordpress_id
+                ? -1
+                : 0
             )
-          })}
-      </div>
+            .map(i => {
+              const date = new Date(i.node.date)
+              return (
+                <a class="news-card" href={`/post/${i.node.slug}`}>
+                  <div class="news-picture">
+                    <img
+                      src={
+                        i.node.featured_media.localFile.childImageSharp.fluid
+                          .src
+                      }
+                    />
+                  </div>
+                  <div class="news-text">
+                    <p class="date">
+                      -{monthNames[date.getMonth()]} {date.getDate()},{" "}
+                      {date.getFullYear()}
+                    </p>
+                    <p class="excerpt">{i.node.acf.excerpt}</p>
+                  </div>
+                </a>
+              )
+            })}
+        </div>
+       { !hasSearchResults&&<button className="showMore" onClick={increaseNumberOfPosts}>
+          Show more <br /> &#xfe40;
+        </button>}
       </div>
     </Layout>
   )
