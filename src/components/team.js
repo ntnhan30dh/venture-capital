24db86753b0601f6972ead7fc9de21723da325ab
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2020-12-11 16:45:44 +0100

    working desktop v

--- src/components/team.js
+++ src/components/team.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react"
+import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 const Team = () => {
-  const [inHover, setHover] = useState(false)
   const data = useStaticQuery(graphql`
     {
       allWordpressWpTeam {
@@ -32,45 +31,53 @@ const Team = () => {
       }
     }
   `)
+  const teamArr = data.allWordpressWpTeam.edges.sort((a, b) =>
+    a.node.date < b.node.date
+      ? 1
+      : b.node.wordpress_id > a.node.wordpress_id
+      ? -1
+      : 0
+  )
+
   return (
     <div id="team" class="team container">
       <div class="section-title">
         <h2>OUR TEAM</h2>
       </div>
       <div class="team-cont">
-        {data.allWordpressWpTeam.edges
-          .sort((a, b) =>
-            a.node.date < b.node.date
-              ? 1
-              : b.node.wordpress_id > a.node.wordpress_id
-              ? -1
-              : 0
-          )
-          //.slice(0,1)
-          .map(i => (
-            <div
-              class="team-card"
-              onMouseEnter={() => setHover(true)}
-              onMouseLeave={() => setHover(false)}
-            >
-              <div className="team-card_left">
-                <div class="team-picture">
-                  <img
-                    src={
-                      i.node.featured_media.localFile.childImageSharp.fluid.src
-                    }
-                  />
-                </div>
-                <div class="team-text">
-                  <p class="name">{i.node.title}</p>
-                  <p class="role">{inHover &&i.node.acf.job_title}</p>
-                </div>
+        {teamArr.map(i => (
+          <div class="team-card ">
+            <div className="team-card_left  ">
+              <div class="team-picture">
+                <img
+                  src={
+                    i.node.featured_media.localFile.childImageSharp.fluid.src
+                  }
+                />
               </div>
-              <div className="team-card_right">
-                {/* <p class="role">{ i.node.acf.biography}</p> */}
+              <div class="team-text">
+                <p class="name">{i.node.title}</p>
+                <p class="role">{i.node.acf.job_title}</p>
               </div>
             </div>
-          ))}
+            <div className="team-card_right">
+              <div className="pic-text">
+              <div class="team-picture">
+                <img
+                  src={
+                    i.node.featured_media.localFile.childImageSharp.fluid.src
+                  }
+                />
+              </div>
+              <div class="team-text">
+                <p class="name">{i.node.title}</p>
+                <p class="role">{i.node.acf.job_title}</p>
+              </div>
+              </div>
+              <p class="bio">{i.node.acf.biography}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
