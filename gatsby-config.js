aac447753b8c78b041d4f9be799d1cc99089d52f
Trong Nhan Nguyen <trong.nguyen@tq933wj62l.ad.deliveryhero.com>
2022-11-01 17:56:53 +0100

    test2

--- gatsby-config.js
+++ gatsby-config.js
@@ -162,37 +162,7 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-PCWT8GP",
-  
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: true,
-  
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-  
-        // Specify optional GTM environment details.
-      //  gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-       // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-       // dataLayerName: "YOUR_DATA_LAYER_NAME",
-  
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-      routeChangeEventName: "virtualPageView",
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    
     'gatsby-plugin-postcss',
     {
       resolve: "gatsby-plugin-google-tagmanager",
